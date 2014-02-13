5c575055eadd18c5d05446e91920c23212b97c05
joeherold <>
2014-02-13 10:22:18 +0100

    Added the funtionality repairFileSystemDatabase()

--- classes/UUIDWorker.php
+++ classes/UUIDWorker.php
@@ -34,69 +34,70 @@ class UUIDWorker extends \Contao\Controller {
      * Update all FileTree fields
      */
     public function updateFileTreeFields() {
-        $this->logString .= 'initated...';
-        $arrFiles = array();
+        if ((\Input::get('create') != '' )) {
+            $this->logString .= 'initated...';
+            $arrFiles = array();
 
 
-        foreach (scan(TL_ROOT . '/system/modules') as $strModule) {
-            $strDir = 'system/modules/' . $strModule . '/dca';
+            foreach (scan(TL_ROOT . '/system/modules') as $strModule) {
+                $strDir = 'system/modules/' . $strModule . '/dca';
 
-            if (!is_dir(TL_ROOT . '/' . $strDir)) {
-                continue;
-            }
-
-            foreach (scan(TL_ROOT . '/' . $strDir) as $strFile) {
-                // Ignore non PHP files and files which have been included before
-                if (substr($strFile, -4) != '.php' || in_array($strFile, $arrFiles)) {
+                if (!is_dir(TL_ROOT . '/' . $strDir)) {
                     continue;
                 }
 
-                $arrFiles[] = substr($strFile, 0, -4);
-            }
-        }
+                foreach (scan(TL_ROOT . '/' . $strDir) as $strFile) {
+                    // Ignore non PHP files and files which have been included before
+                    if (substr($strFile, -4) != '.php' || in_array($strFile, $arrFiles)) {
+                        continue;
+                    }
 
-        $arrFields = array();
-
-        // Find all fileTree fields
-        foreach ($arrFiles as $strTable) {
-            try {
-                $this->loadDataContainer($strTable);
-            } catch (\Exception $e) {
-                $this->logStringErrors .= '<div class="tl_red">'.$e->getMessage().'</div>';
-                continue;
+                    $arrFiles[] = substr($strFile, 0, -4);
+                }
             }
 
-            $arrConfig = &$GLOBALS['TL_DCA'][$strTable]['config'];
+            $arrFields = array();
 
-            // Skip non-database DCAs
-            if ($arrConfig['dataContainer'] == 'File') {
-                continue;
-            }
-            if ($arrConfig['dataContainer'] == 'Folder' && !$arrConfig['databaseAssisted']) {
-                continue;
-            }
+            // Find all fileTree fields
+            foreach ($arrFiles as $strTable) {
+                try {
+                    $this->loadDataContainer($strTable);
+                } catch (\Exception $e) {
+                    $this->logStringErrors .= '<div class="tl_red">' . $e->getMessage() . '</div>';
+                    continue;
+                }
 
-            // Make sure there are fields (see #6437)
-            if (is_array($GLOBALS['TL_DCA'][$strTable]['fields'])) {
-                foreach ($GLOBALS['TL_DCA'][$strTable]['fields'] as $strField => $arrField) {
-                    // FIXME: support other field types
-                    if ($arrField['inputType'] == 'fileTree') {
-                        if ($this->Database->fieldExists($strField, $strTable, true)) {
-                            $key = $arrField['eval']['multiple'] ? 'multiple' : 'single';
-                            $arrFields[$key][] = $strTable . '.' . $strField;
-                        }
+                $arrConfig = &$GLOBALS['TL_DCA'][$strTable]['config'];
 
-                        // Convert the order fields as well
-                        if (isset($arrField['eval']['orderField']) && isset($GLOBALS['TL_DCA'][$strTable]['fields'][$arrField['eval']['orderField']])) {
-                            if ($this->Database->fieldExists($arrField['eval']['orderField'], $strTable, true)) {
-                                $arrFields['order'][] = $strTable . '.' . $arrField['eval']['orderField'];
+                // Skip non-database DCAs
+                if ($arrConfig['dataContainer'] == 'File') {
+                    continue;
+                }
+                if ($arrConfig['dataContainer'] == 'Folder' && !$arrConfig['databaseAssisted']) {
+                    continue;
+                }
+
+                // Make sure there are fields (see #6437)
+                if (is_array($GLOBALS['TL_DCA'][$strTable]['fields'])) {
+                    foreach ($GLOBALS['TL_DCA'][$strTable]['fields'] as $strField => $arrField) {
+                        // FIXME: support other field types
+                        if ($arrField['inputType'] == 'fileTree') {
+                            if ($this->Database->fieldExists($strField, $strTable, true)) {
+                                $key = $arrField['eval']['multiple'] ? 'multiple' : 'single';
+                                $arrFields[$key][] = $strTable . '.' . $strField;
+                            }
+
+                            // Convert the order fields as well
+                            if (isset($arrField['eval']['orderField']) && isset($GLOBALS['TL_DCA'][$strTable]['fields'][$arrField['eval']['orderField']])) {
+                                if ($this->Database->fieldExists($arrField['eval']['orderField'], $strTable, true)) {
+                                    $arrFields['order'][] = $strTable . '.' . $arrField['eval']['orderField'];
+                                }
                             }
                         }
                     }
                 }
             }
         }
-
 
         // Update the existing singleSRC entries
         if (isset($arrFields['single']) && (\Input::get('create') == 'singleSRC')) {
@@ -117,7 +118,81 @@ class UUIDWorker extends \Contao\Controller {
             }
         }
 
+        if ((\Input::get('repair') == 'tl_files')) {
+            $this->repairFileSystemDatabase();
+        }
+
         return true;
+    }
+
+    public function repairFileSystemDatabase() {
+        // Check whether there are UUIDs
+        if (!$this->Database->fieldExists('uuid', 'tl_files')) {
+            try {
+                if (!$this->Database->fieldExists('uuid', 'tl_files')) {
+                    // Adjust the DB structure
+                    $this->Database->query("ALTER TABLE `tl_files` ADD `uuid` binary(16) NULL");
+                    $this->Database->query("ALTER TABLE `tl_files` ADD UNIQUE KEY `uuid` (`uuid`)");
+                }
+            } catch (\Exception $e) {
+                $this->logStringErrors .= '<div class="tl_red">' . $e->getMessage() . '</div>';
+                $this->logString .= '<br><strong class="tl_red">ERROR on updating with new UUID value</strong>';
+            }
+
+            try {
+                if (!$this->Database->fieldExists('pid_backup', 'tl_files')) {
+                    // Backup the pid column and change the column type
+                    $this->Database->query("ALTER TABLE `tl_files` ADD `pid_backup` int(10) unsigned NOT NULL default '0'");
+                    $this->Database->query("UPDATE `tl_files` SET `pid_backup`=`pid`");
+                    $this->Database->query("ALTER TABLE `tl_files` CHANGE `pid` `pid` binary(16) NULL");
+                    $this->Database->query("UPDATE `tl_files` SET `pid`=NULL");
+                    $this->Database->query("UPDATE `tl_files` SET `pid`=NULL WHERE `pid_backup`=0");
+                }
+            } catch (\Exception $e) {
+                $this->logStringErrors .= '<div class="tl_red">' . $e->getMessage() . '</div>';
+                $this->logString .= '<br><strong class="tl_red">ERROR on creating tl_files.pid_backup an setting values</strong>';
+            }
+
+            /*
+             * first we will create all UUIDs in tl_files.uuid
+             */
+            $objFiles = $this->Database->query("SELECT id FROM tl_files");
+
+            // Generate the UUIDs
+            while ($objFiles->next()) {
+                $this->Database->prepare("UPDATE tl_files SET uuid=? WHERE id=?")
+                        ->execute($this->Database->getUuid(), $objFiles->id);
+            }
+
+            $objFiles = $this->Database->query("SELECT pid_backup FROM tl_files WHERE pid_backup>0 GROUP BY pid_backup");
+
+
+            /*
+             * next we will create all pid.UUIDs in tl_files.pid
+             */
+            // Adjust the parent IDs
+            while ($objFiles->next()) {
+                if (($objFiles->pid_backup) > 0) {
+                    $objParent = $this->Database->prepare("SELECT uuid FROM tl_files WHERE id=?")
+                            ->execute($objFiles->pid_backup);
+
+                    if ($objParent->numRows < 1) {
+
+                        $this->logStringErrors .= '<div class="tl_red">' . $e->getMessage() . '</div>';
+                        $this->logString .= '<br><strong class="tl_red">Invalid parent ID ' . $objFiles->pid_backup . '</strong>';
+                        continue;
+                    }
+
+                    $this->Database->prepare("UPDATE tl_files SET pid=? WHERE pid_backup=?")
+                            ->execute($objParent->uuid, $objFiles->pid_backup);
+                }
+            }
+
+
+
+            // Drop the pid_backup column
+            $this->Database->query("ALTER TABLE `tl_files` DROP `pid_backup`");
+        }
     }
 
     /**
@@ -141,7 +216,7 @@ class UUIDWorker extends \Contao\Controller {
                 $objDatabase->query("ALTER TABLE `$table` CHANGE `$field` `$field` binary(16) NULL");
                 $this->logString .= '<br>Field: <strong>' . $field . '</strong> <span class="tl_blue">converted</span> to binary(16).';
             } catch (\Exception $e) {
-                $this->logStringErrors .= '<div class="tl_red">'.$e->getMessage().'</div>';
+                $this->logStringErrors .= '<div class="tl_red">' . $e->getMessage() . '</div>';
                 $this->logString .= '<br><strong class="tl_red">ERROR on creating backup table-field and field conversion to binary(16)...</strong>';
             }
         }
@@ -164,7 +239,7 @@ class UUIDWorker extends \Contao\Controller {
                                     ->execute($objFile->uuid, $objRow->id);
                             $this->logString .= '<br>- Record with id <strong class="tl_green">' . $objRow->id . '</strong> updated.';
                         } catch (\Exception $e) {
-                            $this->logStringErrors .= '<div class="tl_red">'.$e->getMessage().'</div>';
+                            $this->logStringErrors .= '<div class="tl_red">' . $e->getMessage() . '</div>';
                             $this->logString .= '<br><strong class="tl_red">ERROR on updating with new UUID value</strong>';
                         }
 
@@ -201,7 +276,7 @@ class UUIDWorker extends \Contao\Controller {
                 $objDatabase->query("ALTER TABLE `$table` CHANGE `$field` `$field` blob NULL");
                 $this->logString .= '<br>Field: <strong>' . $field . '</strong> <span class="tl_blue">converted</span> to blob.';
             } catch (\Exception $e) {
-                $this->logStringErrors .= '<div class="tl_red">'.$e->getMessage().'</div>';
+                $this->logStringErrors .= '<div class="tl_red">' . $e->getMessage() . '</div>';
                 $this->logString .= '<br><strong class="tl_red">ERROR on creating backup table-field and field conversion to blob...</strong>';
             }
         }
@@ -238,7 +313,7 @@ class UUIDWorker extends \Contao\Controller {
                         $this->logString .= '<br>- Record with id <strong class="tl_green">' . $objRow->id . '</strong> updated.';
                     }
                 } catch (\Exception $e) {
-                    $this->logStringErrors .= '<div class="tl_red">'.$e->getMessage().'</div>';
+                    $this->logStringErrors .= '<div class="tl_red">' . $e->getMessage() . '</div>';
                     $this->logString .= '<br><strong class="tl_red">ERROR on updating with new UUID value</strong>';
                 }
             }
